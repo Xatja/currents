2a0607952e7396f52d01726f0f39dd86828847ce
Katja <xatja@protonmail.com>
2021-07-18 16:46:47 +0200

    Updated tests

--- tests/linkedlisttests.c
+++ tests/linkedlisttests.c
@@ -6,19 +6,29 @@
 
 #include "../source/linkedlist.h"
 
-static void list_add_node(void **state) {
-    List *sut = build_list();
-    int expected = 10;
-    
-    add(sut, expected);
-    int result = value_at_index(sut, 0);
+static void list_int_add_node(void **state) {
+    (void**) state;
 
-    assert_int_equal(expected, result);
+    list_t_int *sut = build_list_int();
+    int item = 100;
+    add_int(sut, &item);
+    assert_int_equal(size_int(sut), 1);
+    destroy_int(sut);
+}
+
+static void list_float_add_node(void **state) {
+    (void**) state;
+
+    list_t_float *sut = build_list_float();
+    float item = 100.0f;
+    add_float(sut, &item);
+    assert_int_equal(size_float(sut), 1);
+    destroy_float(sut);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(list_add_node),
+        cmocka_unit_test(list_int_add_node),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
